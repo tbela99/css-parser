c3df4b64221f75ac184751a6bc3f192ca10b9e1d
Thierry Bela <bnanga@gmail.com>
2023-08-16 04:46:36 -0400

    missing import #8

--- test/specs/block.web-spec.js
+++ test/specs/block.web-spec.js
@@ -1,6 +1,6 @@
 /* generate from test/specs/block.web-spec.ts */
 import {expect as f} from '../../node_modules/@esm-bundle/chai/esm/chai.js';
-import {transform, render} from '../../dist/web/index.js';
+import {transform, render, parse} from '../../dist/web/index.js';
 
 describe('parse block', function () {
 
