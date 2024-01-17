6ced3fe075d6edf844ab00a17835f93f08000698
Thierry Bela <bnanga@gmail.com>
2024-01-16 22:12:06 -0500

    add missing tests #23

--- test/specs/code/index.js
+++ test/specs/code/index.js
@@ -1,16 +1,17 @@
 
 export * as angle from './angle.js';
+export * as block from './block.js';
 export * as calc from './calc.js';
 export * as color from './color.js';
-export * as block from './block.js';
 export * as expand from './expand.js';
 export * as grid from './grid.js';
 export * as import1 from './import1.js';
 export * as import2 from './import2.js';
 export * as import3 from './import3.js';
+export * as malformed from './malformed.js';
 export * as nesting from './nesting.js';
 export * as shorthand from './shorthand.js';
-export * as malformed from './malformed.js';
+export * as sourceMap from './sourcemaps.js';
 export * as vars from './vars.js';
 export * as visitors from './visitors.js';
 export * as walk from './walk.js';
