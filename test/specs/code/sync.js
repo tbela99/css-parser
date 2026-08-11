1dfb7f00baee30a43a04f8b185707bf0cd17e887
Thierry Bela Nanga <bnanga@gmail.com>
2026-08-11 17:44:54 -0400

    add missing files #140

---
+++ test/specs/code/sync.js
@@ -0,0 +1,26 @@
+export function run(
+    describe,
+    expect,
+    it,
+    transform,
+    parse,
+    render,
+    dirname,
+    readFile,
+    resolve,
+    ColorType,
+    EnumToken,
+    ModuleCaseTransformEnum,
+    ModuleScopeEnumOptions,
+    transformSync,
+) {
+    describe("transform sync", function () {
+        it("transform sync #1", function () {
+            return readFile(import.meta.dirname + "/../../files/css/tailwind.css", "utf-8").then((css) =>
+                Promise.all([transform(css), transformSync(css)]).then(([result1, result2]) =>
+                    expect(result1.code).equals(result2.code),
+                ),
+            );
+        });
+    });
+}
